6dff5fc0f2e61ce797d093c81d6f7ca976c01991
Morgan Jacquet <jacquetmorganmj@gmail.com>
2022-02-18 12:24:38 +0100

    add index & strucutre

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="assets/css/bootstrap.min.css" rel="stylesheet">
+    <link href="assets/css/custom.css" rel="stylesheet">
+    <title>Bienvenue chez GSB</title>
+</head>
+<body>
+    <div class="container">
+        <header class="d-flex flex-wrap justify-content-center py-3 mb-4 border-bottom">
+        <a href="/" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-dark text-decoration-none">
+            <img src="assets/img/logo-gsb.png" height="50">
+        </a>
+
+        <ul class="nav nav-pills">
+            <li class="nav-item"><a href="#" class="nav-link active" aria-current="page">Bienvenue</a></li>
+        </ul>
+        </header>
+    </div>
+    <script src="assets/js/bootstrap.min.js"></script>
+</body>
+</html>
