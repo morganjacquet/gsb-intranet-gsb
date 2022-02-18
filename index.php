4ba0e309a5f92b6f5b4760265bbcf233f9c49c06
Morgan Jacquet <jacquetmorganmj@gmail.com>
2022-02-18 15:56:22 +0100

    update index

--- index.php
+++ index.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" type="image/png" href="assets/img/favicon.png" />
     <link href="assets/css/bootstrap.min.css" rel="stylesheet">
     <link href="assets/css/custom.css" rel="stylesheet">
     <title>Bienvenue chez GSB</title>
@@ -19,6 +20,16 @@
             <li class="nav-item"><a href="#" class="nav-link active" aria-current="page">Bienvenue</a></li>
         </ul>
         </header>
+
+        <div class="row">
+            <div class="col-5 align-self-center">
+                <h3>Bienvenue sur le réseau GSB</h3>
+                <p>Le laboratoire Galaxy Swiss Bourdin (GSB) géant pharmaceutique leader de ce secteur industriel.</p>
+            </div>
+            <div class="col-7 text-center">
+                <img src="assets/img/banner.png" height="400">
+            </div>
+        </div>
     </div>
     <script src="assets/js/bootstrap.min.js"></script>
 </body>
